376f8f8ea30154320bea0c043da41683004b0c88
Raphael Dozzi <rdozzi84@gmail.com>
2020-08-16 20:41:19 -0700

    feat: Added getActiveSection + setSectionAsActive
    Added two functions to obtain the active section criterion.
    getActiveSection is a helper function that defines the
    viewscreen criteria
    
    setSectionAsActive uses that helper function and implements changes
    the active section through the scroll event
    
    Also refactored comments for neatness.

--- js/app.js
+++ js/app.js
@@ -1,39 +1,34 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+// Manipulating the DOM exercise.
+// Exercise programmatically builds navigation,
+// scrolls to anchors from navigation,
+// and highlights section in viewport upon scrolling.
+// Dependencies: None
+// JS Version: ES2015/ES6
+// JS Standard: ESlint
 
-/**
- * Define Global Variables
- * 
-*/
+// Define Global Variables
+
 const navigationBar = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+// End Global Variables
+// Start Helper Functions
 
+// This function provides the criteria for the active section
+const getActiveSection = function() {
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+    for(let section of sections){
+        const sectionBounding = section.getBoundingClientRect();
+        if (sectionBounding.top >= -500 && sectionBounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)){
+            return section;
+        }
+    }
+}
 
-// build the nav
+// End Helper Functions
+// Begin Main Functions
+
+// This function builds the navigator bar based on the sections present
 const navBuilder = function() {
 
     let navigationMenu = '';
@@ -50,8 +45,9 @@ const navBuilder = function() {
     navigationBar.innerHTML = navigationMenu;
 }
 
-// Scroll to anchor ID using scrollTO event
+// This function scrollIntoView event
 
+// This function adds the smooth scroll feature
 const navigationLinks = document.getElementsByClassName('menu__link');
 
 const scrollHandler = function() {
@@ -64,14 +60,44 @@ const scrollHandler = function() {
     }
 }
 
+// Add class 'active' to button when pressed
+
+// Add class 'active' to section while scrolling
+const setSectionAsActive = function() {
+    window.addEventListener('scroll', function (event) {
+        let section = getActiveSection();
+        //Added an empty statement for dead space that won't be highlihgted by the JS
+        if(section === undefined){
+            ;
+        }
+        else{//sets one section as active and removes active from other sections where applicable
+            section.classList.add('your-active-class');
+            for (let item of sections) {
+                if (item.id != section.id && item.classList.contains('your-active-class')) {
+                    item.classList.remove('your-active-class');
+                }
+
+            }
+        }
+    });
+};
 
 
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+        // // set corresponding header style
+        // const active = document.querySelector('li[data-nav="' + section.id + '"]');
+        // active.classList.add('active__link');
+        // // remove from other headers
+        // const headers = document.querySelectorAll('.menu__link');
+        // for (let item of headers) {
+        //     console.log(item);
+        //     if (item.dataset.nav != active.dataset.nav & item.classList.contains('active__link')) {
+        //         item.classList.remove('active__link');
+        //     }
+        // };
+
+// End Main Functions
+// Begin Events 
 
 // Build menu 
 navBuilder();
@@ -80,6 +106,6 @@ navBuilder();
 scrollHandler();
 
 // Set sections as active
-
+setSectionAsActive();
 
 
